ba7e24050716936c4453c50be9d420558793f591
carlos <carlos@LAPTOP-MP192ADI.technicolor.net>
2021-06-20 20:09:33 +0200

    Subida a repositorio local

---
+++ ProyectoERMASTER2/src/proyecto/Despedida.java
@@ -0,0 +1,14 @@
+package proyecto;
+
+public class Despedida {
+
+	public Despedida() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Esta es la despedida de CarlosSagrado");
+
+	}
+
+}
